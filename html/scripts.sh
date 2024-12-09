d05728bda4a231e9966059398d567a00dda43845
Afsal Thaj <afsal.taj06@gmail.com>
2024-12-09 19:10:15 +1100

    Cleanup

--- html/scripts.sh
+++ html/scripts.sh
@@ -1,6 +1,18 @@
 # Adding a cart content
 ~/projects/resolve/golem/target/debug/golem-cli api-deployment delete localhost:9006
 ~/projects/resolve/golem/target/debug/golem-cli api-definition update  ~/projects/resolve/golem-worker-gateway-examples/html/api-definition.yaml --def-format yaml
-~/projects/resolve/golem/target/debug/golem-cli api-deployment deploy --host localhost:9006 --definition html-example/0.0.37
+~/projects/resolve/golem/target/debug/golem-cli api-deployment deploy --host localhost:9006 --definition html-example/0.0.1
 curl -X POST -H "Content-Type: application/json" -d @/Users/afsalthaj/projects/resolve/golem-worker-gateway-examples/html/request.json http://localhost:9006/v37/1/add-item
 http://localhost:9006/v37/1/get-cart-contents
+
+# Get the used component
+~/projects/resolve/golem/target/debug/golem-cli component get --component-name afsal
+
+# For Loop and Loop Reduction
+# i.e, same example
+
+# CORS
+# enabling cors
+
+# Component Constraints Demo
+~/projects/resolve/golem/target/debug/golem-cli component update --component-name afsal test-components/auction.wasm
