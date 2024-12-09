7c4a03324cfd6b7da00312e088b35ac8547d032b
Afsal Thaj <afsal.taj06@gmail.com>
2024-12-09 19:57:08 +1100

    Add CORS

--- html/scripts.sh
+++ html/scripts.sh
@@ -19,16 +19,6 @@ http://localhost:9006/v37/1/get-cart-contents
 curl -X OPTIONS http://localhost:9006/v1/afsal/hello  -H "Access-Control-Request-Method: GET" -v
 curl -X GET http://localhost:9006/v1/afsal/hello -v
 
-fetch('http://localhost:9006/v1/afsal/hello', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-})
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-
 # Component Constraints Demo
 ~/projects/resolve/golem/target/debug/golem-cli component update --component-name afsal test-components/auction.wasm
 
