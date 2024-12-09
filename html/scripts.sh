e0f5280f2e0c26243da684029c8e188f12d3fde5
Afsal Thaj <afsal.taj06@gmail.com>
2024-12-09 19:11:43 +1100

    Add client id and secret

--- html/scripts.sh
+++ html/scripts.sh
@@ -5,6 +5,10 @@
 curl -X POST -H "Content-Type: application/json" -d @/Users/afsalthaj/projects/resolve/golem-worker-gateway-examples/html/request.json http://localhost:9006/v37/1/add-item
 http://localhost:9006/v37/1/get-cart-contents
 
+
+# Security Scheme addition
+./target/debug/golem-cli api-security-scheme create --scheme.id shopping-cart-security-scheme --provider.type google --client.id $GOOGLE_CLIENT_ID --client.secret $GOOGLE_CLIENT_SECRET --redirect.url http://localhost:9006/auth/callback --scopes openid,email,profile
+
 # Get the used component
 ~/projects/resolve/golem/target/debug/golem-cli component get --component-name afsal
 
@@ -16,3 +20,4 @@ http://localhost:9006/v37/1/get-cart-contents
 
 # Component Constraints Demo
 ~/projects/resolve/golem/target/debug/golem-cli component update --component-name afsal test-components/auction.wasm
+
